0b74a1260eb8474cc8ea67506f14e2888dd8f84d
anhptp0812 <aphuc049@gmail.com>
2024-11-08 21:50:20 +0700

    sửa detail đơn hàng chi tiết

--- Audio_TripleV/src/main/java/com/example/demo/controller/DonHangController.java
+++ Audio_TripleV/src/main/java/com/example/demo/controller/DonHangController.java
@@ -20,13 +20,7 @@ import com.example.demo.service.SanPhamChiTietService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -236,7 +230,13 @@ public class DonHangController {
         donHang.setTongGia(totalAmount);
         donHangRepository.save(donHang);
 
-        return "redirect:/user/don-hang";
+        return "redirect:/user/ban-hang";
+    }
+
+    @GetMapping("ban-hang/details/{id}")
+    @ResponseBody
+    public List<DonHangChiTiet> getDonHangDetails(@PathVariable Integer id) {
+        return donHangChiTietRepository.findByDonHang_Id(id);
     }
 
 
--- Audio_TripleV/src/main/java/com/example/demo/service/DonHangService.java
+++ Audio_TripleV/src/main/java/com/example/demo/service/DonHangService.java
@@ -1,9 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.DonHang;
+import com.example.demo.entity.DonHangChiTiet;
 import com.example.demo.entityCustom.DonHangRepository;
+import com.example.demo.repository.DonHangChiTietRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DonHangService {
@@ -11,9 +15,15 @@ public class DonHangService {
     @Autowired
     private DonHangRepository donHangRepository;
 
+    @Autowired
+    private DonHangChiTietRepository donHangChiTietRepository;
+
     public DonHang findByid(Integer id) {
         return donHangRepository.findById(id).orElse(null);
     }
 
+    public List<DonHangChiTiet> findByDHid(Integer id) {
+        return donHangChiTietRepository.findByDonHang_Id(id);
+    }
 
 }
