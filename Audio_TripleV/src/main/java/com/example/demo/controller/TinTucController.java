417cb1ac87462c64d584a4744f887df6ad6fe1c2
ThanhNMT04 <thanhhnmph39897@fpt.edu.vn>
2024-11-22 11:55:36 +0700

    Update html
    khách hàng đăng nhập hiển thị được tên

--- Audio_TripleV/src/main/java/com/example/demo/controller/TinTucController.java
+++ Audio_TripleV/src/main/java/com/example/demo/controller/TinTucController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.KhachHang;
 import com.example.demo.entity.SanPhamChiTiet;
+import com.example.demo.repository.KhachHangRepository;
 import com.example.demo.service.SanPhamChiTietService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("khach-hang")
@@ -17,8 +22,18 @@ public class TinTucController {
     @Autowired
     SanPhamChiTietService sanPhamChiTietService;
 
+    @Autowired
+    KhachHangRepository khachHangRepository;
+
     @GetMapping("/trang-chu/hien-thi")
-    public String showTop4SanPhamMoiAndSanPhamXemNhieuNhat(Model model) {
+    public String showTop4SanPhamMoiAndSanPhamXemNhieuNhat(@AuthenticationPrincipal User user, Model model) {
+        String username = user.getUsername();
+        Optional<KhachHang> optionalKhachHang = khachHangRepository.findByTaiKhoan(username);
+        if (optionalKhachHang.isPresent()) {
+            KhachHang khachHang = optionalKhachHang.get();
+            model.addAttribute("fullName", khachHang.getTen());
+        }
+
         List<SanPhamChiTiet> newestProducts = sanPhamChiTietService.getTop4NewestProducts();
         model.addAttribute("sanPhamChiTietList", newestProducts);
         List<SanPhamChiTiet> sanPhamChiTietList = sanPhamChiTietService.getTop4SanPhamChiTiet();
