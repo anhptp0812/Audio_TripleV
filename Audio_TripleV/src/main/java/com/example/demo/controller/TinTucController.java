8a8683bc51d4df13e0fb089fdd4b086286af42c0
anhptp0812 <aphuc049@gmail.com>
2024-12-02 13:05:54 +0700

    sửa lại bootstrap và vài thứ lặt vặt

--- Audio_TripleV/src/main/java/com/example/demo/controller/TinTucController.java
+++ Audio_TripleV/src/main/java/com/example/demo/controller/TinTucController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("khach-hang")
+@RequestMapping("/khach-hang")
 public class TinTucController {
     @Autowired
     SanPhamChiTietService sanPhamChiTietService;
